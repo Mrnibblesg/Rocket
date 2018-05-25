413e1134004fa13d6140ba385eea7a0565d1aa6e
Mrnibblesg <mrnibblesgutierrez@gmail.com>
2018-05-24 20:27:44 -0700

    to string (rgb and rgba), random bright colors
    New functions that can return an rgb or rgba string, and a static function that returns a new random color object, with all color values above the dimmest parameter. Also some information about the constructor at the top of the file.

--- Color.js
+++ Color.js
@@ -1,4 +1,14 @@
-function Color(r,g,b,a){
+//The color constructor allows for easy use and manipulation of rgba color strings.
+//red, green, blue, and alpha properties are stored just by the first letter.
+//After you change any of them and want to use the new color, use constructCol().
+//If you want to create a new color object from an existing rgb or rgba string, use Color.createFromRgba(color),
+//where color is the rgb or rgba string, and a new color object will be returned.
+
+
+
+//Create a new color. r = red value, g = green value, b = blue value, and a = alpha value.
+//The alpha value defaults to 1 (no transparency).
+function Color(r,g,b,a=1){
     if (!(typeof r === 'number') || !(typeof g === 'number') || !(typeof b === 'number')){
         
         console.error(`Color constructor requires all rgb values to be numbers!
@@ -45,5 +55,17 @@ Color.createFromRgba = function(rgba){
         a = 1;
     }
     return new Color(r,g,b,a);
-    
+}
+Color.rgbToString = function(r,g,b){
+    return 'rgb(' + r + ',' + g + ',' + b + ')';
+}
+Color.rgbaToString = function(r,g,b,a){
+    return 'rgba(' + r + ',' + g + ',' + b + ',' + a + ')';
+}
+
+Color.randomBright = function(dimmest){
+    const r = rand(255,dimmest);
+    const g = rand(255,dimmest);
+    const b = rand(255,dimmest);
+    return new Color(r,g,b);
 }
